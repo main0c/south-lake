e078b39417fb6cb05076cff0970e64b80b8838b5
Philip Dow <phil@phildow.net>
2016-02-15 09:03:22 -0800

    initial commit

---
+++ South Lake/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  South Lake
+//
+//  Created by Philip Dow on 2/15/16.
+//  Copyright © 2016 Phil Dow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
