765821cd4dcd5747c2107bbc5ab06537d8770ead
Philip Dow <phil@phildow.net>
2016-02-20 14:40:47 -0800

    added cached data property to File

--- South Lake/File.swift
+++ South Lake/File.swift
@@ -12,6 +12,36 @@ import Cocoa
 class File: DataSource {
     override class var model_mime_type: NSString { return "private/private" }
     override class var model_uti: NSString { return "private.private" }
+    
+    // The data is saved as an attachment, but cache it
+    // An attachment is available as NSData or a read only file NSURL
+    
+    private var _data: NSData?
+    
+    var data: NSData? {
+        get {
+            guard _data == nil else {
+                return _data
+            }
+            
+            if  let attachment = attachmentNamed("data"),
+                let content = attachment.content {
+                _data = content
+                return _data
+            } else {
+                return nil
+            }
+        }
+        set (value) {
+            if let value = value {
+                setAttachmentNamed("data", withContentType: mime_type, content: value)
+                _data = value
+            } else {
+                removeAttachmentNamed("data")
+                _data = nil
+            }
+        }
+    }
 }
 
 extension File {
