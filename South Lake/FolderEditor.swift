a44eb44f40577627a378a37302cc994f8d1b2ebd
Philip Dow <phil@phildow.net>
2016-03-23 17:51:47 -0700

    prepping for folder editor which is like the library editor

---
+++ South Lake/FolderEditor.swift
@@ -0,0 +1,18 @@
+//
+//  FolderEditor.swift
+//  South Lake
+//
+//  Created by Philip Dow on 3/23/16.
+//  Copyright © 2016 Phil Dow. All rights reserved.
+//
+
+import Cocoa
+
+class FolderEditor: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
